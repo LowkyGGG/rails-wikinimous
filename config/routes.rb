b0063a06fdad1cab5073f08a8ae4df3347d9d5ef
loris morlais <loris.morlais@icloud.com>
2024-11-12 14:58:15 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/articles' => 'articles#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/articles/new' => 'articles#new', as: :new_article
+  post '/articles' => 'articles#create'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/articles/:id/edit' => 'articles#edit', as: :edit_article
+  patch '/articles/:id' => 'articles#update'
+
+  get 'articles/:id' => 'articles#show', as: :article
+
+  delete '/articles/:id' => 'articles#destroy'
 end
